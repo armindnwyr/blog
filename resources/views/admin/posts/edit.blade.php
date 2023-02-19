a29fa3bf9276570da9ec8ee2df61c66c88330c0b
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-02-18 21:30:35 -0500

    image preview ready

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -2,16 +2,31 @@
     @push('css')
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     @endpush
-    <form action="{{ route('admin.posts.update', $post) }}" method="post">
+    <form action="{{ route('admin.posts.update', $post) }}" method="post" enctype="multipart/form-data">
         @csrf
         @method('put')
+        <div class="mb-6 relative">
+            <figure>
+                <img id="imgPreview" src="{{ $post->imageReplace }}"
+                    class="aspect-[16/9] w-full object-cover object-center rounded-lg">
+            </figure>
+
+            <div class="absolute top-8 right-8">
+                <label class="flex items-center px-4 py-2 bg-white rounded-lg">
+                    <i class="fa-solid fa-camera mr-3"></i>
+                    Actualizar imagen
+                    <input type="file" class="hidden" accept="image/*" onchange="previewImage(event, '#imgPreview')">
+                </label>
+            </div>
+        </div>
+
         <div class="bg-white rounded p-6 shadow-lg">
             <x-validation-errors class="mb-4" />
 
             <div class="mb-4">
                 <x-label>Titulo</x-label>
-                <x-input type="text" class="w-full" name="name" id="name" value="{{ old('name', $post->name) }}"
-                    onkeyup="string_to_slug()" />
+                <x-input type="text" class="w-full" name="name" id="name"
+                    value="{{ old('name', $post->name) }}" onkeyup="string_to_slug()" />
             </div>
 
             <div class="mb-4">
@@ -48,7 +63,7 @@
                     @endforeach --}}
 
                     @foreach ($post->tags as $tag)
-                    <option value="{{ $tag->id }}" selected>{{ $tag->name }}</option>
+                        <option value="{{ $tag->id }}" selected>{{ $tag->name }}</option>
                     @endforeach
                 </select>
             </div>
@@ -61,10 +76,13 @@
             <div class="mb-4">
                 <input type="hidden" value="0" name="publish">
                 <label class="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" name="publish" value="1" class="sr-only peer" @checked(old('publish', $post->publish)==1)>
-                    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    <input type="checkbox" name="publish" value="1" class="sr-only peer"
+                        @checked(old('publish', $post->publish) == 1)>
+                    <div
+                        class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                    </div>
                     <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-900">¿Quieres publicar?</span>
-                  </label>
+                </label>
             </div>
             {{-- button update   --}}
             <div class="flex justify-end">
@@ -108,16 +126,16 @@
             $(document).ready(function() {
                 $('.js-example-basic-multiple').select2({
                     ajax: {
-                        url: '{{ route('tags.select2')}}',
-                        dataType:'json',
+                        url: '{{ route('tags.select2') }}',
+                        dataType: 'json',
                         delay: 250,
-                        data: function(params){
+                        data: function(params) {
                             return {
                                 term: params.term
                             }
                         },
 
-                        processResults: function(data){
+                        processResults: function(data) {
                             return {
                                 results: data
                             }
@@ -125,6 +143,28 @@
                     }
                 });
             });
+
+            function previewImage(event, querySelector) {
+
+                //Recuperamos el input que desencadeno la acción
+                const input = event.target;
+
+                //Recuperamos la etiqueta img donde cargaremos la imagen
+                $imgPreview = document.querySelector(querySelector);
+
+                // Verificamos si existe una imagen seleccionada
+                if (!input.files.length) return
+
+                //Recuperamos el archivo subido
+                file = input.files[0];
+
+                //Creamos la url
+                objectURL = URL.createObjectURL(file);
+
+                //Modificamos el atributo src de la etiqueta img
+                $imgPreview.src = objectURL;
+
+            }
         </script>
     @endpush
 </x-admin-layout>
